804ff3f9f8433e578e7971b43a6e936f33c3ff6e
mys3lf <mys3lf@recalot.com>
2016-04-20 14:16:44 +0200

    Added default host

--- js/app/app.js
+++ js/app/app.js
@@ -365,7 +365,7 @@
                     $rootScope.hosts = JSON.parse(hosts);
                     $rootScope.requests.host = $rootScope.hosts.active.host;
                 } else {
-                    $rootScope.hosts = {active: $rootScope.requests.host, history: [$rootScope.requests.defaultHost]};
+                    $rootScope.hosts = {active: $rootScope.requests.defaultHost, history: [$rootScope.requests.defaultHost]};
                     localStorage.setItem("hosts", JSON.stringify($rootScope.hosts));
                 }
             });
